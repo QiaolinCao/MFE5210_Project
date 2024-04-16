29a9227b936d685842f02a413debc06df5cb5772
QiaolinCao <151534116+QiaolinCao@users.noreply.github.com>
2024-04-16 19:54:42 +0800

    trader模块（w）

---
+++ trader/setting.py
@@ -0,0 +1,47 @@
+"""
+交易框架全局设定
+"""
+
+from logging import CRITICAL
+from typing import Dict, Any
+from tzlocal import get_localzone_name
+
+from .utility import load_json
+
+SETTINGS: Dict[str, Any] = {
+    "font.family": "微软雅黑",
+    "font.size": 12,
+
+    "log.active": True,
+    "log.level": CRITICAL,
+    "log.console": True,
+    "log.file": True,
+
+    "email.server": "smtp.qq.com",
+    "email.port": 465,
+    "email.username": "",
+    "email.password": "",
+    "email.sender": "",
+    "email.receiver": "",
+
+    "datafeed.name": "",
+    "datafeed.username": "",
+    "datafeed.password": "",
+
+    "database.timezone": get_localzone_name(),
+    "database.name": "mysql",
+    "database.database": "mfe5210_cta",
+    "database.host": "localhost",
+    "database.port": 3306,
+    "database.user": "root",
+    "database.password": "caoqiaolin327"
+}
+
+# Load global setting from json file.
+SETTING_FILENAME: str = "vt_setting.json"
+SETTINGS.update(load_json(SETTING_FILENAME))
+
+
+def get_settings(prefix: str = "") -> Dict[str, Any]:
+    prefix_length: int = len(prefix)
+    return {k[prefix_length:]: v for k, v in SETTINGS.items() if k.startswith(prefix)}
